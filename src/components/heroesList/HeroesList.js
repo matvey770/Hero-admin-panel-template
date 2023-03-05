c120c1327b9ba7193dad1ad713a91059c12d0c36
Matvey <smirnv.matvejj@yandex.ru>
2023-03-05 20:50:33 +0300

    added redux-thunk, middleware, store enhancer

---
+++ src/components/heroesList/HeroesList.js
@@ -0,0 +1,81 @@
+import {useHttp} from '../../hooks/http.hook';
+import { useCallback, useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { createSelector } from 'reselect';
+
+import { fetchHeroes, heroDeleted } from '../../actions';
+import HeroesListItem from "../heroesListItem/HeroesListItem";
+import Spinner from '../spinner/Spinner';
+
+// Задача для этого компонента:
+// При клике на "крестик" идет удаление персонажа из общего состояния
+// Усложненная задача:
+// Удаление идет и с json файла при помощи метода DELETE
+
+const HeroesList = () => {
+
+    const filteredHeroesSelector = createSelector(
+        (state) => state.filters.activeFilter,
+        (state) => state.heroes.heroes,
+        (filter, heroes) => {
+            if (filter === 'all') {
+                return heroes
+            } else {
+                return heroes.filter(item => item.element === filter)
+            }
+        }
+    );
+
+    // const filteredHeroes = useSelector(state => {
+    //     if (state.filters.activeFilter === 'all') {
+    //         return state.heroes.heroes
+    //     } else {
+    //         return state.heroes.heroes.filter(item => item.element === state.filters.activeFilter)
+    //     }
+    // })
+
+    const filteredHeroes = useSelector(filteredHeroesSelector);
+    const heroesLoadingStatus = useSelector(state => state.heroes.heroesLoadingStatus);
+    const dispatch = useDispatch();
+    const {request} = useHttp();
+
+    useEffect(() => {
+        dispatch(fetchHeroes(request))
+        // eslint-disable-next-line
+    }, []);
+
+
+    const onDelete = useCallback((id) => {
+        request(`http://localhost:3001/heroes/${id}`, "DELETE")
+            .then(data => console.log(data, 'Deleted'))
+            .then(dispatch(heroDeleted(id)))
+            .catch(err => console.log(err));
+        // eslint-disable-next-line  
+    }, [request]);
+
+    if (heroesLoadingStatus === "loading") {
+        return <Spinner/>;
+    } else if (heroesLoadingStatus === "error") {
+        return <h5 className="text-center mt-5">Ошибка загрузки</h5>
+    }
+
+
+    const renderHeroesList = (arr) => {
+        if (arr.length === 0) {
+            return <h5 className="text-center mt-5">Героев пока нет</h5>
+        }
+
+        return arr.map(({id, ...props}) => {
+            return <HeroesListItem key={id} {...props} onDelete={() => onDelete(id)}/>
+        })
+    }
+    
+    const elements = renderHeroesList(filteredHeroes);
+    return (
+        <ul>
+            {elements}
+        </ul>
+    )
+}
+
+export default HeroesList;
